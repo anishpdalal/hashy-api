4665857e9c97af6c3143594d5de69048361d2ab3
Anish Dalal <anishdalal@Anishs-MacBook-Pro.local>
2022-07-05 16:08:46 -0400

    initial version of zendesk indexer

--- worker/app.py
+++ worker/app.py
@@ -1,16 +1,17 @@
+import datetime
 import itertools
 import json
 import os
-import re
+import unicodedata
 import uuid
+import re
 
+from bs4 import BeautifulSoup
 import pinecone
+import pysbd
 import requests
 from sentence_transformers import SentenceTransformer
 from sqlalchemy import create_engine, text
-
-TAG_RE = re.compile(r'<[^>]+>')
-REGEX_EXP = r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s"
 
 
 def chunks(iterable, batch_size=100):
@@ -45,89 +46,89 @@ def get_credentials(source):
     return subdomain, access_token
 
 
-def get_user_emails(subdomain, access_token):
-    bearer_token = f"Bearer {access_token}"
-    header = {'Authorization': bearer_token}
-    url = f"https://{subdomain}/api/v2/users.json?role[]=agent&role[]=admin"
-    data = requests.get(url, headers=header).json()
-    user_emails = [r["email"] for r in data.get("users", [])]
-    return user_emails
-
-
-def update_user_emails(engine, user_emails, source_id):
-    with engine.connect() as connection:
-        user_emails_str = ", ".join(user_emails)
-        connection.execute(text(f"update source set shared_with = '{user_emails_str}' where id = '{source_id}'"))
-    
-
-def get_zendesk_help_center_articles(subdomain, access_token, source_id):
-    url = f"https://{subdomain}/api/v2/help_center/articles.json?sort_by=updated_at&sort_order=desc"
+def get_zendesk_help_center_article(subdomain, access_token, source_id, article_id):
+    url = f"https://{subdomain}/api/v2/help_center/articles/{article_id}.json"
     bearer_token = f"Bearer {access_token}"
     header = {'Authorization': bearer_token}
     data = requests.get(url, headers=header).json()
-    articles = []
-    for article in data.get("articles", []):
-        body_text = article.get("body")
-        if not body_text:
-            continue
-        articles.append({
-            "id": f"{subdomain}-{article['id']}",
-            "text": article["title"],
-            "source_id": str(source_id),
-            "doc_type": "zendesk_help_center_article",
-            "doc_last_updated": article["updated_at"],
-            "doc_url": article["url"],
-            "doc_name": article["title"],
-            "doc_labels": article.get("label_names", []),
-            "vote_sum": article["vote_sum"],
-        })
-        cleaned_body_text = TAG_RE.sub('', body_text)
-        chunks = [chunk for chunk in re.split(REGEX_EXP, cleaned_body_text) if any(c.isalpha() for c in chunk)]
-        chunk_size = 4
-        for idx in range(0, len(chunks), chunk_size):
-            chunk = chunks[idx:idx+chunk_size]
-            chunk_str = " ".join(chunk)
-            articles.append({
-                "id": f"{subdomain}-{article['id']}-{idx}",
-                "text": chunk_str,
+    article = data["article"]
+    article_title = article["title"]
+    article_url = article["url"]
+    article_last_updated = article["updated_at"]
+    article_body = article["body"]
+    article_labels = article.get("label_names", [])
+    soup = BeautifulSoup(article_body, "html.parser")
+    seg = pysbd.Segmenter(language="en", clean=True)
+    headers = soup.find_all(re.compile('^h[1-6]$'))
+    results = []
+    results.append({
+        "id": f"{subdomain}-{article_id}",
+        "display_text": article_title,
+        "text_to_index": article_title,
+        "source_id": str(source_id),
+        "doc_type": "zendesk_hc_article_title",
+        "doc_last_updated": article_last_updated,
+        "doc_url": article_url,
+        "doc_name": article_title,
+        "doc_labels": article_labels,
+    })
+    if headers:
+        header_locations = [0] + [article_body.find(str(header)) for header in headers]
+        for idx, loc in enumerate(header_locations):
+            if idx == len(header_locations) - 1:
+                display_text = unicodedata.normalize("NFKD", article_body[loc:])
+            else:
+                display_text = unicodedata.normalize("NFKD", article_body[loc:header_locations[idx+1]])
+            text_to_index = " ".join(seg.segment(display_text))
+            results.append({
+                "id": f"{subdomain}-{article_id}-{idx}",
+                "display_text": display_text,
+                "text_to_index": text_to_index,
                 "source_id": str(source_id),
-                "doc_type": "zendesk_help_center_article",
-                "doc_last_updated": article["updated_at"],
-                "doc_url": article["url"],
-                "doc_name": article["title"],
-                "doc_labels": article.get("label_names", []),
-                "vote_sum": article["vote_sum"],
+                "doc_type": "zendesk_hc_article_body",
+                "doc_last_updated": article_last_updated,
+                "doc_url": article_url,
+                "doc_name": article_title,
+                "doc_labels": article_labels,
             })
-    return articles
+    else:
+        chunks = seg.segment(text)
+        chunk_size = 5
+        for idx, i in enumerate(range(0, len(chunks), chunk_size)):
+            chunk = chunks[i:i+chunk_size]
+            text_to_index = " ".join(chunk)
+            results.append({
+                "id": f"{subdomain}-{article_id}-{idx}",
+                "display_text": text_to_index,
+                "text_to_index": text_to_index,
+                "source_id": str(source_id),
+                "doc_type": "zendesk_hc_article_body",
+                "doc_last_updated": article_last_updated,
+                "doc_url": article_url,
+                "doc_name": article_title,
+                "doc_labels": article_labels,
+            })
+    return results
 
 
-def index_zendesk_help_center_articles(index, bi_encoder, articles):
-    article_text_embeddings = bi_encoder.encode([article["text"] for article in articles]).tolist()
+def index_zendesk_help_center_articles(index, bi_encoder, results):
+    text_embeddings = bi_encoder.encode([result["text_to_index"] for result in results]).tolist()
     upsert_data_generator = map(lambda i: (
-        articles[i]["id"],
-        article_text_embeddings[i],
+        results[i]["id"],
+        text_embeddings[i],
         {
-            "text": articles[i]["text"][0:1500],
-            "source_id": articles[i]["source_id"],
-            "doc_type": articles[i]["doc_type"],
-            "doc_last_updated": articles[i]["doc_last_updated"],
-            "doc_url": articles[i]["doc_url"],
-            "doc_name": articles[i]["doc_name"],
-            "doc_labels": articles[i]["doc_labels"],
-            "vote_sum": articles[i]["vote_sum"],
-        }), range(len(articles))
+            "text": results[i]["display_text"],
+            "source_id": results[i]["source_id"],
+            "doc_type": results[i]["doc_type"],
+            "doc_last_updated": results[i]["doc_last_updated"],
+            "doc_url": results[i]["doc_url"],
+            "doc_name": results[i]["doc_name"],
+            "doc_labels": results[i]["doc_labels"],
+        }), range(len(results))
     )
     for ids_vectors_chunk in chunks(upsert_data_generator, batch_size=100):
         index.upsert(vectors=ids_vectors_chunk)
 
-
-# with engine.connect() as connection:
-#                     document = connection.execute(
-#                         text(f"select id from document where doc_id = '{article['id']}' and owner = '{source['owner']}' and doc_type = 'zendesk_help_center_article'")
-#                     ).fetchone()
-#                     if not document:
-#                         connection.execute(text(f"insert into document (owner, name, type, doc_id, doc_last_updated) values ({source['owner']}, {article['title']}, 'zendesk_help_center_article', {article['id']}, {article['updated_at']}::timestamp with TIME ZONE)"))
-        
 
 def handler(event, context):
     PINECONE_KEY = os.environ["PINECONE_KEY"]
@@ -145,9 +146,19 @@ def handler(event, context):
             subdomain, access_token = get_credentials(source)
             if not subdomain and not access_token:
                 continue
-            user_emails = get_user_emails(subdomain, access_token)
-            update_user_emails(engine, user_emails, source_id)
-            articles = get_zendesk_help_center_articles(subdomain, access_token, source_id)
-            index_zendesk_help_center_articles(index, bi_encoder, articles)
-
+            article_id = record_body["article_id"]
+            results = get_zendesk_help_center_article(subdomain, access_token, source_id, article_id)
+            index_zendesk_help_center_articles(index, bi_encoder, results)
+        with engine.connect() as connection:
+            document = connection.execute(
+                text(f"select id from document where doc_id = '{article_id}' and owner = '{source['owner']}' and type = 'zendesk_help_center_article'")
+            ).fetchone()
+            article_name = results[0]["doc_name"]
+            article_last_updated = results[0]["doc_last_updated"]
+            current = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            if not document:
+                doc_id = str(uuid.uuid4())
+                connection.execute(text(f"insert into document (id, owner, name, type, doc_id, doc_last_updated, created) values ('{doc_id}', '{source['owner']}', '{article_name}', 'zendesk_help_center_article', '{article_id}', '{article_last_updated}'::timestamp with TIME ZONE, '{current}'::timestamp with TIME ZONE)"))
+            else:
+                connection.execute(text(f"update document SET updated = '{current}'::timestamp with TIME ZONE where id = '{document.id}'"))
     engine.dispose()
