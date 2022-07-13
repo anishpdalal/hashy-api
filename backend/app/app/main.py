a7c0f2c83c38e36d4bcb0cb861d7eb024cfd6277
Anish Dalal <anishdalal@Anishs-MacBook-Pro.local>
2022-07-12 21:48:44 -0400

    create production api

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 from app.api.api_v1.api import api_router
 
 api = FastAPI()
 
 api.include_router(api_router, prefix="/v0")
+
+handler = Mangum(api, lifespan="off")
